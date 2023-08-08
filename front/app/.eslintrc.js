924de44008395c594cb498849c5e21c6f8dbacdd
tokun4869 <ba.tokun4869@gmail.com>
2023-08-08 10:58:35 +0900

    全ファイルの追跡

---
+++ front/app/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest/globals": true,
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json",
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "jest",
+        "prettier",
+    ],
+    "rules": {
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error"],
+        "prettier/prettier": "error",
+    },
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    },
+  }
