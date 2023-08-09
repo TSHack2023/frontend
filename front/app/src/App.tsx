fa14ab300843f73ceb695e22305091f5ae634139
yuma-ngo <takagi-yuma-nd@ynu.jp>
2023-08-09 10:38:44 +0900

    homeの修正,reviewのレイアウト追加

--- front/app/src/App.tsx
+++ front/app/src/App.tsx
@@ -20,7 +20,7 @@ const App = (): JSX.Element => {
           <Route path="/Signup">
             <Signup />
           </Route>
-          <Route path="/Home/">
+          <Route path="/Home/detail">
             <HomeDetail />
           </Route>
           <Route path="/Home">
@@ -29,7 +29,7 @@ const App = (): JSX.Element => {
           <Route path="/Contribute">
             <Contribute />
           </Route>
-          <Route path="/Review/">
+          <Route path="/Review/a">
             <ReviewDetail />
           </Route>
           <Route path="/Review">
