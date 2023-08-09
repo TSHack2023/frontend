47133debbab180c699b015b2d813a9c5dceacb56
tokun4869 <ba.tokun4869@gmail.com>
2023-08-09 12:12:38 +0900

    Azureへのアップロード関数の修正

--- front/app/src/components/azureStorage.tsx
+++ front/app/src/components/azureStorage.tsx
@@ -1,30 +1,43 @@
-import { BlobServiceClient } from '@azure/storage-blob';
+import { BlobServiceClient } from "@azure/storage-blob";
 
-const account = process.env.REACT_APP_AZURE_ACCOUNT_NAME || 'account';
-const sas_token = process.env.REACT_APP_AZURE_SHARED_ACCESS_SIGNATURE || 'sas_token';
+const account = process.env.REACT_APP_AZURE_ACCOUNT_NAME ?? "account";
+const sasToken =
+  process.env.REACT_APP_AZURE_SHARED_ACCESS_SIGNATURE ?? "sasToken";
 
 const blobServiceClient = new BlobServiceClient(
-    `https://${account}.blob.core.windows.net${sas_token}`
+  `https://${account}.blob.core.windows.net${sasToken}`,
 );
 
-const containerName = process.env.REACT_APP_AZURE_CONTAINER_NAME || 'container';
+const containerName = process.env.REACT_APP_AZURE_CONTAINER_NAME ?? "container";
 
- async function uploadFile2AzureStorage(file: any) {
-    const containerClient = blobServiceClient.getContainerClient(containerName);
+const uploadFile2AzureStorage = async (file: File): Promise<string> => {
+  const containerClient = blobServiceClient.getContainerClient(containerName);
 
-    const content = file;
-    const blobName = 'react_newblob' + new Date().getTime();
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    const blobOptions = { blobHTTPHeaders: { blobContentType: 'application/pdf' } }; // this is where you set all blob options, including the header and content-type
-    const uploadBlobResponse = await blockBlobClient.upload(content, content.size, blobOptions);
-    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
-    console.log(
-        uploadBlobResponse._response.request.requestId,
-        uploadBlobResponse._response.request.url.replace(sas_token, '')
-    );
-    const file_url = uploadBlobResponse._response.request.url.replace(sas_token, '');
+  const content = file;
+  const blobName = "react_newblob" + new Date().getTime().toString();
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const blobOptions = {
+    blobHTTPHeaders: { blobContentType: "File" },
+  }; // this is where you set all blob options, including the header and content-type
+  const uploadBlobResponse = await blockBlobClient.upload(
+    content,
+    content.size,
+    blobOptions,
+  );
+  console.log(
+    `Upload block blob ${blobName} successfully`,
+    uploadBlobResponse.requestId,
+  );
+  console.log(
+    uploadBlobResponse._response.request.requestId,
+    uploadBlobResponse._response.request.url.replace(sasToken, ""),
+  );
+  const fileUrl = uploadBlobResponse._response.request.url.replace(
+    sasToken,
+    "",
+  );
 
-    return file_url;
-}
+  return fileUrl;
+};
 
 export default uploadFile2AzureStorage;
